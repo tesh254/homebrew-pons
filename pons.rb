099989d6de1321c0fe34f4f03152e146c1e49963
GitHub Action <action@github.com>
2025-09-08 19:57:19 +0000

    Update pons to v1.0.4 (BuildInfo support)

--- pons.rb
+++ pons.rb
@@ -1,15 +1,15 @@
 class Pons < Formula
   desc "A robust CLI tool used to organize and automate complex workflows with templated commands"
   homepage "https://github.com/tesh254/pons"
-  version "1.0.6"
+  version "1.0.4"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/tesh254/pons/releases/download/v0.0.1/pons-darwin-amd64"
-      sha256 "b2a862e4d232bb48c68714aa40a39b50ab9e15fc53f6390b4c6b02f1159b8e72"
+      url "https://github.com/tesh254/pons/releases/download/v1.0.4/pons-darwin-amd64"
+      sha256 "8b124f3fb134289bd5453170c4ab1e3afe2e40e2861c26d920b116a445120fb7"
     else
-      url "https://github.com/tesh254/pons/releases/download/v0.0.1/pons-darwin-arm64"
-      sha256 "eeaf1f0911cef029ffdbac221e321f24fc48e66025ba747a4e541f9f1bf49129"
+      url "https://github.com/tesh254/pons/releases/download/v1.0.4/pons-darwin-arm64"
+      sha256 "f46aab0bb66a18081bf696924e5d3f29caadda4212ed974eebdcb620e83df68f"
     end
   end
 
@@ -23,5 +23,14 @@ class Pons < Formula
     # Test basic version command
     system "#{bin}/pons", "--version"
     system "#{bin}/pn", "--version"
+    
+    # Test BuildInfo version commands
+    assert_match version.to_s, shell_output("#{bin}/pons version --short")
+    assert_match "Platform:", shell_output("#{bin}/pons version")
+    
+    # Test JSON output is valid
+    json_output = shell_output("#{bin}/pons version --json")
+    assert_match "version", json_output
+    assert_match "platform", json_output
   end
 end
