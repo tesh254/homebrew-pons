e3cbd1a65f034449026e489fe5820094c1613192
GitHub Action <action@github.com>
2025-09-16 20:50:57 +0000

    Update pons to v1.0.16 (BuildInfo support)

--- pons.rb
+++ pons.rb
@@ -1,15 +1,15 @@
 class Pons < Formula
   desc "A robust CLI tool used to organize and automate complex workflows with templated commands"
   homepage "https://github.com/tesh254/pons"
-  version "1.0.15"
+  version "1.0.16"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/tesh254/pons/releases/download/v1.0.15/pons-darwin-amd64"
-      sha256 "96644a577c5f41c8fa0cb6f98286f7c10cec2dab65f92e7cb68918101307b97f"
+      url "https://github.com/tesh254/pons/releases/download/v1.0.16/pons-darwin-amd64"
+      sha256 "7f848b4a21d13f114ef63b4f7dcf8ada9ef2b304e8c058a193ba5a4e866d82e7"
     else
-      url "https://github.com/tesh254/pons/releases/download/v1.0.15/pons-darwin-arm64"
-      sha256 "12274ab29bc70e8142d5836998e3a2d23dea12e37175ac753fec8be9a1d51189"
+      url "https://github.com/tesh254/pons/releases/download/v1.0.16/pons-darwin-arm64"
+      sha256 "9e3ad3eec55f6e07db3dcf45bf2b802bb0bab0d851e0c17b370de13e795a0ab1"
     end
   end
 
